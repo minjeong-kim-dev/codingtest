c8f871cd1aa56a1ac9562401defc81bcb4986b02
minjeong <min970401@gmail.com>
2025-12-04 13:56:00 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,14 @@
+x = int(input())
+n = int(input())
+
+result = 0
+
+for i in range(1, n+1):
+    a,b = map(int, input().split())
+
+    result += a * b
+
+if result == x:
+    print('Yes')
+else:
+    print('No')
