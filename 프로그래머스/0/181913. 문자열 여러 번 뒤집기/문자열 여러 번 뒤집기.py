659d3db28b03054643200727a639662c72485927
minjeong <min970401@gmail.com>
2026-01-12 20:13:35 +0900

    Update 문자열 여러 번 뒤집기.py

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,10 +1,11 @@
 def solution(my_string, queries):
     answer = ''
-    arr = list(my_string)
+    arr = list(my_string)                    # my_string list로 변경
     
-    for s,e in queries:
-        arr[s : e+1] = arr[s : e+1][::-1]
-        
-    answer = ''.join(arr)        
+    for s,e in queries:                     
+        arr[s : e+1] = arr[s : e+1][::-1]    # s부터 e까지 역순으로 만들기
+    
+    answer = ''.join(arr)                    # list를 문자열로 만들어주기
+            
     
     return answer
