9bf2e8e50344d5c41cfb971a2396da4b6a47bbfe
minjeong <min970401@gmail.com>
2026-01-25 19:19:28 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    
+    for i in range(len(myString)):
+        if myString[i] == 'a' or myString[i] == 'A':
+            answer += 'A'
+        else:
+            answer += myString[i].lower()
+    
+    return answer
