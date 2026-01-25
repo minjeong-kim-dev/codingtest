cf4d18d36038f3de182ea4745f55c0752fe9aa03
minjeong <min970401@gmail.com>
2026-01-25 11:31:04 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 882.66 ms, Memory: 30.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -0,0 +1,17 @@
+def solution(arr):
+    count = 0
+      
+    while True:
+        new_arr = arr.copy()
+        
+        for i in range(len(arr)):
+            if arr[i] % 2 == 0 and arr[i] >= 50:
+                arr[i] //= 2
+
+            elif arr[i] % 2 == 1 and arr[i] < 50:
+                arr[i] = (arr[i] * 2) +1  
+
+        if arr == new_arr:
+            return count
+            
+        count += 1
