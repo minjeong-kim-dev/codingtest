f2fec3d43f0b7f72c7813dcdad9f13029dc03f8f
minjeong <min970401@gmail.com>
2026-01-25 19:45:58 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.72 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
@@ -0,0 +1,11 @@
+def solution(my_string, alp):
+    answer = ''
+    
+    for i in my_string:
+        if alp in my_string:
+            answer = my_string.replace(alp, alp.upper())
+        else:
+            answer = my_string
+
+        
+    return answer
