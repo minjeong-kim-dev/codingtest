b0953d99a2384c275e6907e0025d0286ea5ac653
minjeong <min970401@gmail.com>
2026-01-27 22:50:59 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 285.49 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -1,11 +1,3 @@
 def solution(arr, k):
     answer = []
-    
-    for i in arr:
-        if i not in answer and len(answer) < k:
-            answer.append(i)
-            
-    while len(answer) < k:
-        answer.append(-1)
-    
     return answer
