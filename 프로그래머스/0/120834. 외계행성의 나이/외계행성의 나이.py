5cbaf266fa6310865b23fdaa5205dcde07b1512c
minjeong <min970401@gmail.com>
2026-02-18 14:04:04 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,13 @@
+def solution(age):
+    answer = ''
+    str_age = str(age)
+    alpa = {
+        '0':'a','1':'b','2':'c','3':'d',
+        '4':'e','5':'f','6':'g','7':'h',
+        '8':'i','9':'j'
+    }
+    
+    for i in str_age:
+        answer += alpa[i] 
+    
+    return answer
