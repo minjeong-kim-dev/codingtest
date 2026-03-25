014f151dc07cdf4235193ae0e187fa8e6f000a02
minjeong <min970401@gmail.com>
2026-03-25 11:46:19 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,4 +1,10 @@
 def solution(array):
-
+    max_value = array[0]
+    max_index = 0
     
-    return [max(array), array.index(max(array))]
+    for i, num in enumerate(array):
+        if num >= max_value:
+            max_value = num
+            max_index = i
+        
+    return [max_value, max_index]
