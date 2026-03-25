5bd06ebf2768f71f80441e2f481a1c9ca42f3463
minjeong <min970401@gmail.com>
2026-03-25 21:47:58 +0900

    [level 0] Title: 암호 해독, Time: 0.10 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,8 +1,7 @@
 def solution(cipher, code):
     answer = ''
     
-    for i in range(len(cipher)):
-        if (i + 1) % code == 0:
-            answer += cipher[i]
+    for i in range(code-1, len(cipher), code):
+        answer += cipher[i]
     
     return answer
