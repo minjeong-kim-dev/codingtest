5c1267c789506e1539d264633e1b76fdfd1f239a
minjeong <min970401@gmail.com>
2026-06-09 10:03:43 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,13 +1,16 @@
 def solution(a, b):
     answer = 0
     
-    answer1 = str(a)+str(b)
-    answer2 = str(b)+str(a)
+    str_a = str(a)
+    str_b = str(b)
     
-    if answer1 >  answer2:
-        answer = int(answer1)
+    if (str_a + str_b) > (str_b + str_a):
+        answer = str_a + str_b
         
-    else:
-        answer = int(answer2)
+    elif (str_a + str_b) < (str_b + str_a):
+        answer = str_b + str_a
     
-    return answer
+    elif (str_a + str_b) == (str_b + str_a):
+        answer = str_a + str_b
+    
+    return int(answer)
