f8fbf48ce82abde06c224044b6ceed36bda9e930
minjeong <min970401@gmail.com>
2026-06-10 11:45:01 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.20 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,16 +1,16 @@
 def solution(numLog):
     answer = ''
-    
-    for i in range(1, len(numLog)):     # numLog[1]부터 numLog의 길이만큼 반복
-        diff = numLog[i] - numLog[i-1]  # ex) numLOg[1]과 numLog[2]의 차이
-                   
-        if diff == +1:
+
+    for i in range(1, len(numLog)):
+        diff = numLog[i] - numLog[i-1]
+
+        if diff == 1:
             answer += 'w'
         elif diff == -1:
             answer += 's'
-        elif diff == +10:
-            answer += 'd'        
+        elif diff == 10:
+            answer += 'd'
         elif diff == -10:
-            answer += 'a'            
-    
+            answer += 'a'
+
     return answer
