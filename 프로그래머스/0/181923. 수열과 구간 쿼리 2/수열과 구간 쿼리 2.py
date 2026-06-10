994131abcec3bd1dc8e40109e763a001bde6bab2
minjeong <min970401@gmail.com>
2026-06-10 11:26:10 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.95 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -2,15 +2,16 @@ def solution(arr, queries):
     answer = []
     
     for s, e, k in queries:
-        temp = []                       # 새로운 list 추가
+        arr_ = arr[s:e+1]
+        candidate = []
         
-        for i in range(s, e+1):
-            if arr[i] > k:              # arr[i]가 k보다 클때 
-                temp.append(arr[i])     # temp에 arr[i]추가
-                            
-        if not temp:                    # temp가 비어있을때
+        for i in arr_:
+            if i > k:       
+                candidate.append(i)           
+        if candidate:
+            answer.append(min(candidate))
+
+        else:  
             answer.append(-1)
-        else:                           # temp가 비어있지 않을때
-            answer.append(min(temp))    # temp에서 제일 작은 값을 answer에 추가
-    
+
     return answer
