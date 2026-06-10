d6968f2b8db427468aa1335b3ef2b6ed85bd9272
minjeong <min970401@gmail.com>
2026-06-10 10:21:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,11 +1,8 @@
 def solution(num_list):
     
-
     if num_list[-1] > num_list[-2]:
-        answer = num_list[-1] - num_list[-2]
-        num_list.append(answer)
+        num_list.append(num_list[-1]-num_list[-2])
     else:
-        answer = num_list[-1] * 2
-        num_list.append(answer)
+        num_list.append(num_list[-1] *2)
     
     return num_list
